8ec65dfa69dfd595e97baba0f034f66f33d53f41
Ayoub El Barji <aelbarji@groupehn.com>
2015-11-13 09:53:22 +0100

    Initial Commit

---
+++ Caceis/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Caceis
+//
+//  Created by Ayoub El barji on 13/11/2015.
+//  Copyright © 2015 Groupehn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Caceis/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Caceis
+//
+//  Created by Ayoub El barji on 13/11/2015.
+//  Copyright © 2015 Groupehn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
